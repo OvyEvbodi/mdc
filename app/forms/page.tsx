b56bdbe43e4ced770cb3000350eda4a84f380045
Ovy Evbodi <evbodiovo@gmail.com>
2025-05-11 23:12:52 +0100

    Fix: Change api url for all forms

--- app/forms/page.tsx
+++ app/forms/page.tsx
@@ -5,6 +5,7 @@ import MDCForm from "@/components/Form";
 import { redirect } from "next/navigation";
 // import { Button } from "@/components/ui/button";
 import { cookies } from "next/headers";
+import { ErrorBoundary } from "react-error-boundary";
 
 
 export const metadata: Metadata = {
@@ -19,7 +20,7 @@ const FormsPage = async() => {
   const session = await auth();
   if (!session) redirect("/");
   
-  const result = await fetch("/api/forms", {
+  const result = await fetch("https://mdc-nu.vercel.app/api/forms", {
     method: "GET",
     body: null,
     headers: {
@@ -38,13 +39,18 @@ const FormsPage = async() => {
     <main className=" min-h-screen p-8 md:pt-12 flex flex-col justify-center items-center">
       <div className="max-w-6xl flex flex-col justify-center items-center">
         <h1 className="p-4 text-3xl font-bold">Forms page</h1>
-        <section className="flex justify-center items-center flex-wrap gap-4 md:gap-8 ">
-          {
-            forms && forms.map(form => (
-              <MDCForm key={form.id} {...form} />
-            ))
-          }
-        </section>
+        <div className="flex justify-center items-center">
+        <ErrorBoundary fallback={<div className="text-center">Something&apos;s not right. Please refresh the page.</div>}>
+          <section className="flex justify-center items-center flex-wrap gap-4 md:gap-8 ">
+            {
+              forms && forms.map(form => (
+                <MDCForm key={form.id} {...form} />
+              ))
+            }
+          </section>
+        </ErrorBoundary>
+        </div>
+        
       </div>
     </main>
   )
